62b44d5eb4815076355d244947640b201c91ee15
felipe89 <gfelipe.trindade@gmail.com>
2020-06-15 18:07:10 -0300

    Adicionando Regra C

--- src/main/java/Entidade/CalculaTaxasImpl.java
+++ src/main/java/Entidade/CalculaTaxasImpl.java
@@ -1,5 +1,7 @@
 package Entidade;
 
+import sun.font.DelegatingShape;
+
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
@@ -17,8 +19,7 @@ public class CalculaTaxasImpl implements CalculaTaxas {
             BigDecimal porcentagem = new BigDecimal(3.0);
 
             valorTotalTaxa = porcentagem.divide(new BigDecimal(100)).multiply(valor).add(valorTaxa);
-        }
-        else if (taxa.descricao.equals("B")) {
+        } else if (taxa.descricao.equals("B")) {
             BigDecimal valorDentroPeriodo = new BigDecimal(10.0);
             BigDecimal valorForaPeriodo = new BigDecimal(8.00);
 
@@ -26,12 +27,10 @@ public class CalculaTaxasImpl implements CalculaTaxas {
 
             if (dias <= 30) {
                 valorTotalTaxa = new BigDecimal(10.0);
-            }
-            else {
+            } else {
                 valorTotalTaxa = new BigDecimal(8.00);
             }
-        }
-        else if (taxa.descricao.equals("C")){
+        } else if (taxa.descricao.equals("C")) {
             BigDecimal porcentagemAcima30 = new BigDecimal(1.2);
             BigDecimal porcentagemAte30 = new BigDecimal(2.1);
             BigDecimal porcentagemAte25 = new BigDecimal(4.3);
@@ -44,29 +43,73 @@ public class CalculaTaxasImpl implements CalculaTaxas {
 
             if (dias <= 5) {
                 valorTotalTaxa = porcentagemAte5.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias <= 10){
+            } else if (dias <= 10) {
                 valorTotalTaxa = porcentagemAte10.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias <= 15){
+            } else if (dias <= 15) {
                 valorTotalTaxa = porcentagemAte15.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias <= 20){
+            } else if (dias <= 20) {
                 valorTotalTaxa = porcentagemAte20.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias <= 25){
+            } else if (dias <= 25) {
                 valorTotalTaxa = porcentagemAte25.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias <= 30){
+            } else if (dias <= 30) {
                 valorTotalTaxa = porcentagemAte30.divide(new BigDecimal(100)).multiply(valor);
-            }
-            else if (dias > 30){
+            } else if (dias > 30) {
                 valorTotalTaxa = porcentagemAcima30.divide(new BigDecimal(100)).multiply(valor);
             }
+        }
+
+        if (taxa.descricao.equals("D")) {
+
+            if (valor.compareTo(BigDecimal.ZERO) <= 25000) {
+                BigDecimal valorTaxa = new BigDecimal(2.00);
+                BigDecimal porcentagem = new BigDecimal(3.0);
+                valorTotalTaxa = porcentagem.divide(new BigDecimal(100)).multiply(valor).add(valorTaxa);
+
+            } else if (valor.compareTo(BigDecimal.ZERO) <= 120000) {
+                BigDecimal valorDentroPeriodo = new BigDecimal(10.0);
+                BigDecimal valorForaPeriodo = new BigDecimal(8.00);
+
+                long dia = ChronoUnit.DAYS.between(dataCadastro, dataTransferencia);
+
+                if (dia <= 30) {
+                    valorTotalTaxa = new BigDecimal(10.0);
+                } else {
+                    valorTotalTaxa = new BigDecimal(8.00);
+                }
+
+            } else if (valor.compareTo(BigDecimal.ZERO) > 120000) {
+
+                BigDecimal porcentoAcima30 = new BigDecimal(1.2);
+                BigDecimal porcentoAte30 = new BigDecimal(2.1);
+                BigDecimal porcentoAte25 = new BigDecimal(4.3);
+                BigDecimal porcentoAte20 = new BigDecimal(5.4);
+                BigDecimal porcentoAte15 = new BigDecimal(6.7);
+                BigDecimal porcentoAte10 = new BigDecimal(7.4);
+                BigDecimal porcentoAte5 = new BigDecimal(8.3);
+
+                long dia = ChronoUnit.DAYS.between(dataCadastro, dataTransferencia);
+
+                if (dia <= 5) {
+                    valorTotalTaxa = porcentoAte5.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia <= 10) {
+                    valorTotalTaxa = porcentoAte10.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia <= 15) {
+                    valorTotalTaxa = porcentoAte15.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia <= 20) {
+                    valorTotalTaxa = porcentoAte20.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia <= 25) {
+                    valorTotalTaxa = porcentoAte25.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia <= 30) {
+                    valorTotalTaxa = porcentoAte30.divide(new BigDecimal(100)).multiply(valor);
+                } else if (dia > 30) {
+                    valorTotalTaxa = porcentoAcima30.divide(new BigDecimal(100)).multiply(valor);
+                }
+
+            }
+
         }
         return valorTotalTaxa;
     }
 }
-
 
 
--- src/main/java/Programa/Programa.java
+++ src/main/java/Programa/Programa.java
@@ -6,7 +6,6 @@ import Entidade.Taxas;
 import Entidade.Transferencia;
 import sun.util.calendar.BaseCalendar;
 import sun.util.calendar.LocalGregorianCalendar;
-
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
@@ -22,17 +21,18 @@ public class Programa {
         Transferencia transferencia = new Transferencia("Conta Felipe", "Conta Nelson", new BigDecimal(1000), LocalDate.now());
         Transferencia transferencia1 = new Transferencia("Conta Felipe", "Conta Nelson", new BigDecimal(1000), LocalDate.of(2020, 06, 1));
         Transferencia transferencia2 = new Transferencia("Conta Felipe", "Conta Nelson", new BigDecimal(1000), LocalDate.of(2020, 06, 22));
-
+        Transferencia transferencia3 = new Transferencia("Conta Felipe", "Conta Nelson", new BigDecimal(1000), LocalDate.of(2020, 06, 25));
 
         CalculaTaxasImpl calculaTaxas = new CalculaTaxasImpl();
         BigDecimal valorTotalTaxaA = calculaTaxas.caculaTaxa(Taxas.TAXA_A,transferencia.getValor(), transferencia.getData());
         BigDecimal valorTotalTaxaB = calculaTaxas.caculaTaxa(Taxas.TAXA_B,transferencia1.getValor(), transferencia1.getData());
         BigDecimal valorTotalTaxaC = calculaTaxas.caculaTaxa(Taxas.TAXA_C,transferencia2.getValor(), transferencia2.getData());
-
+        BigDecimal valorTotalTaxaD = calculaTaxas.caculaTaxa(Taxas.TAXA_D,transferencia3.getValor(), transferencia3.getData());
 
         System.out.println(transferencia.getContaOrigem() + " " + " " + transferencia.getContaDestino() + " " + " " + transferencia.getValor() + " " + " " + "Valor total de taxas " + valorTotalTaxaA.floatValue());
         System.out.println(transferencia1.getContaOrigem() + " " + " " + transferencia1.getContaDestino() + " " + " " + transferencia1.getValor() + " " + " " + "Valor total de taxas " + valorTotalTaxaB.floatValue());
         System.out.println(transferencia2.getContaOrigem() + " " + " " + transferencia2.getContaDestino() + " " + " " + transferencia2.getValor() + " " + " " + "Valor total de taxas " + valorTotalTaxaC.floatValue());
+        System.out.println(transferencia3.getContaOrigem() + " " + " " + transferencia3.getContaDestino() + " " + " " + transferencia3.getValor() + " " + " " + "Valor total de taxas " + valorTotalTaxaD.floatValue());
 
     }
 }
